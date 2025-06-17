4966930567c8682ce9f9ea8f174c8ac9d4fb2234
Petter Hancock <pettor@gmail.com>
2025-06-17 20:47:51 +0200

    feat(storybook): compile and use shared storybook config

--- configs/storybook/vite.config.ts
+++ configs/storybook/vite.config.ts
@@ -1,4 +1,3 @@
-import type { PluginOption } from "vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,10 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(() => {
   return {
     base: "./",
-    plugins: [tailwindcss() as PluginOption, tsconfigPaths() as PluginOption],
+    plugins: [tailwindcss(), tsconfigPaths()],
     // Add storybook-specific dependencies to pre-optimization
     optimizeDeps: {
-      include: ["@storybook/addon-a11y", "@storybook/addon-interactions", "@storybook/addon-viewport"],
+      include: ["@storybook/addon-a11y"],
     },
   };
 });
